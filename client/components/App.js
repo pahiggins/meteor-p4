b802676d5086d29a528ecc407a039bb2cdce96cf
Peter Higgins <pah@cenabo.com>
2018-04-23 12:15:57 +0100

    Add user auth and basic features

--- client/components/App.js
+++ client/components/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import Header from './Header';
 
-export default () => {
+export default (props) => {
   return (
     <div>
       <Header />
+      {props.children}
     </div>
   );
 };
--- client/main.js
+++ client/main.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/App';
+import BinsMain from './components/bins/BinsMain';
+import BinsList from './components/bins/BinsList';
+import { Bins } from '../imports/collections/bins';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <IndexRoute component={BinsList} />
+      <Route path="bins/:binId" component={BinsMain}></Route>
+    </Route>
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<App />, document.querySelector('.render-target'));
+  ReactDOM.render(routes, document.querySelector('.render-target'));
 });
